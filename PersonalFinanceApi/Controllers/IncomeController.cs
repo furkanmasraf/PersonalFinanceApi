922603130ab2a8faa0a390f3fce5304b5c2ad6ab
Hamza Tekel <hamza.tekel12345@gmail.com>
2025-08-22 21:33:21 +0300

    createcontroller

---
+++ PersonalFinanceApi/Controllers/IncomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalFinanceApi.Models;
+using PersonalFinanceApi.DTOs;
+
+namespace PersonalFinanceApi.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IncomeController : ControllerBase
+
+    {
+        private static List<Income> Incomes = new List<Income>();
+    }
+}
---
+++ PersonalFinanceApi/Models/Income.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalFinanceApi.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Source { get; set; }
+        public DateTime DateReceived { get; set; }
+
+    }
+}
